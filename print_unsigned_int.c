c03f84db9f89b908a732d29dea65f78542cf9364
Astro175 <aspark400@gmail.com>
2023-01-15 08:26:39 +0100

    new

--- print_unsigned_int.c
+++ print_unsigned_int.c
@@ -39,7 +39,7 @@ int print_u(va_list u)
  * Return: numbers of digits printed
  */
 
-int print_o (va_list o)
+int print_o(va_list o)
 {
 	unsigned int a[11];
 	unsigned int i, m, n, sum;
@@ -66,4 +66,34 @@ int print_o (va_list o)
 }
 
 /**
- *
+ * print_b - takes an unsigned int and prints it in binary notation
+ * @b: unsigned int to print
+ * Return: number of digits printed
+ */
+
+int print_b(va_list b)
+{
+	unsigned int n, m, i, sum;
+	unsigned int a[32];
+	int count;
+
+	n = va_arg(b, unsigned int);
+	m = 2147483648;
+	a[0] = n / m;
+
+	for (i = 7; i < 32; i++)
+	{
+		m /= 2;
+		a[i] = (n / m) % 2;
+	}
+	for (i = 0, sum = 0, count = 0; i < 32; i++)
+	{
+		sum += a[i];
+		if (sum || i == 31)
+		{
+			_putchar('0' + a[i]);
+			count++;
+		}
+	}
+	return (count);
+}
