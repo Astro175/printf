f652e8275bd7832e654a4ebe794b65433c0dc854
Astro175 <aspark400@gmail.com>
2023-01-16 16:36:55 +0100

    new changes to number functions

--- print_numbers.c
+++ print_numbers.c
@@ -9,42 +9,30 @@
 int print_d(va_list d)
 {
 	int a[10];
-	int j, sum, m, n, count;
+	int j, n, count;
+	int m = 1000000;
 
 	n = va_arg(d, int);
-
 	count = 0;
-	m = 1000000000;
-
-	a[0] = n / m;
-
-	for (j = 0; j < 10; j++)
-	{
-		m /= 10;
-		a[j] = (n / m) % 10;
-	}
 
 	if (n < 0)
 	{
 		_putchar('-');
 		count++;
-		for (j = 0; j < 10; j++)
-		{
-			a[j] *= -1;
-		}
+		n = -n;
 	}
-
-	for (j = 0, sum = 0; j < 10; j++)
+	for (j = 0; j < 10; j++)
 	{
-		sum += a[j];
-		if (sum != 0 || j == 9)
-		{
-			_putchar('0' + a[j]);
-			count++;
-		}
-
+		a[j] = n % 10;
+		n = n / 10;
+		if (n == 0 && j > 0)
+			break;
 	}
-
+	for (j = j - 1; j >= 0; j--)
+	{
+		_putchar('0' + a[j]);
+		count++;
+	}
 	return (count);
 }
 
